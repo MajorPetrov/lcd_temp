c517e01f0befa3ec49a6aa5f31cb06bb6dbc4355
MajorPetrov <razdobreevvlad@live.fr>
2017-10-25 18:45:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Project Title
+
+small thermometer programm with DS18B20 as sensor
+
+## Getting Started
+
+Just open the file in your Arduino IDE, then transfer it to your ESP8266
+
+## Built With
+
+* [Arduino](https://www.arduino.cc/) - The IDE used
+
+## Authors
+
+* **Vladimir Razdobreev** - *Myself* - [MajorPetrov](https://github.com/MajorPetrov)
+
+## License
+
+This project is licensed under the GNU General Public License - see the [LICENSE](LICENSE) file for details
---
+++ lcd_temp.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
+#define ONE_WIRE_BUS 2
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  // start serial port
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+
+  // Start up the library
+  sensors.begin();
+  lcd.begin(20,4);   // initialisation de l'écran à la bonne taille
+  //lcd.backlight(); aussi utile que la bite d'un pape
+  lcd.setCursor(0, 0); //première ligne
+  lcd.print(" Temperature: ");
+}
+ 
+ 
+void loop(void)
+{
+  float temp;
+  // call sensors.requestTemperatures() to issue a global temperature
+  // request to all devices on the bus
+  Serial.print(" Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+
+  Serial.print("Temperature for Device 1 is: ");
+  temp = sensors.getTempCByIndex(0);
+  Serial.print(temp); // Why "byIndex"? 
+    // You can have more than one IC on the same bus. 
+    // 0 refers to the first IC on the wire
+  lcd.setCursor(14, 0);  
+  lcd.print(temp);
+  delay(1000);
+}
